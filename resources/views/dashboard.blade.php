2aa4ebe581364b00984db850c63afa1aaf0b55a9
Mark Oliver Jensen <mark.oliver@adaptagency.com>
2021-05-21 09:28:57 +0200

    Renamed the dashboard to Chats
    WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('My Chats') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h2 class="font-bold text-2xl">My chats</h2>
-                    <ul>
-                        @foreach($user->chats as $chat)
-                            <li><a class="text-red-400" href="/{{ $chat->uuid }}">{{ $chat->name }}</a></li>
-                        @endforeach
-                    </ul>
-                </div>
+                @foreach(\Illuminate\Support\Facades\Auth::user()->chats as $chat)
+                    <div class="p-6 bg-white border-b border-gray-200 flex">
+                        <div class="w-1/2">
+                            <h3 class="font-bold text-xl"><a class="text-red-400"
+                                                             href="/chat/{{ $chat->uuid }}">{{ $chat->name }}</a></h3>
+                        </div>
+                        <div class="w-1/2 text-right">
+                            <ul class="list-none">
+                                @foreach($chat->users as $u)
+                                    <li class="inline-block text-gray-400">
+                                        @if($loop->last)
+                                            {{$u->name}}
+                                        @else
+                                            {{$u->name}},
+                                    </li>
+                                    @endif
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
