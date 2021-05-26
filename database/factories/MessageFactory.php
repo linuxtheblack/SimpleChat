e7f2b2c17e66557f14055bf68c4060f29014db47
Mark Oliver Jensen <mark.oliver@adaptagency.com>
2021-05-26 10:03:07 +0200

    Added factories for Chats and Messages

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Chat;
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MessageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Message::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'message' => $this->faker->sentence,
+            'user_id' => User::inRandomOrder()->first(),
+            'chat_id' => Chat::inRandomOrder()->first()
+        ];
+    }
+}
